3923396eb306b46351fecba9594ef55b81988f0e
antomicus <asevillanozahonero@gmail.com>
2026-02-09 16:14:39 +0100

    Update build.gradle.kts and settings.gradle.kts for improved plugin management and repository configuration
    - Introduced explicit plugin declarations for Android and Kotlin in build.gradle.kts.
    - Simplified repository management in settings.gradle.kts by removing group regex inclusions and setting repositoriesMode to PREFER_SETTINGS.
    - Enhanced clarity and maintainability of build configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,7 @@ buildscript {
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
